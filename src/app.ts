84559ce8d98f188071425c670125914e64b87342
Hamim Howlader <mdhamimhowladerasif@gmail.com>
2025-09-04 12:29:13 +0600

    fix cors error

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://minimal-lms-frontend.vercel.app"],
+    origin: ["http://localhost:3000","https://minimal-lms-frontend-six.vercel.app"],
     credentials: true,
   })
 );
